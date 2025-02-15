7f8c71523bba05548a21255fa08dce2a573aba4b
ShannonJTaylor <shannonjo8475@gmail.com>
2025-02-15 08:39:03 -0500

    Updated routes files and gitignore.

--- server/src/routes/api/weatherRoutes.ts
+++ server/src/routes/api/weatherRoutes.ts
@@ -1,19 +1,26 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
 const router = Router();
 
 // import HistoryService from '../../service/historyService.js';
 // import WeatherService from '../../service/weatherService.js';
 
 // TODO: POST Request with city name to retrieve weather data
-router.post('/', (req, res) => {
+router.post('/', (_req: Request, res: Response) => {
   // TODO: GET weather data from city name
   // TODO: save city to search history
+  res.send("City weather data");
 });
 
 // TODO: GET search history
-router.get('/history', async (req, res) => {});
+router.get('/history', async (_req: Request, res: Response) => {
+  //logic to get search history
+  res.json({ message: "Fetching search history..." });
+});
 
 // * BONUS TODO: DELETE city from search history
-router.delete('/history/:id', async (req, res) => {});
+router.delete('/history/:id', async (req: Request, res: Response) => {
+  //logic to delete city from search history
+  res.json({ message: `Deleting city with ID: ${req.params.id}` });
+});
 
 export default router;
